e5f3cb465e6c797627606319e3a387faa3786a19
Emmanuel Chang <echang@medallia.com>
2024-09-23 21:51:52 -0300

    some refactors, exec online callback only once

--- components/ring_link/heartbeat.c
+++ components/ring_link/heartbeat.c
@@ -1,7 +1,5 @@
 #include "heartbeat.h"
 
-#define HEARTBEAT_INTERVAL_SEC 5
-#define MAX_FAILURES 5  // Number of consecutive failures before considering a board as "out"
 
 static int heartbeat_id = 0;
 static bool heartbeat_received = false;
@@ -11,32 +9,36 @@ static int failure_count = 0;
 
 static const char *TAG = "==> heartbeat";
 
-void offline_board_callback(int failure_count){
-    ESP_LOGE(TAG, "There is an OFFLINE board. Callback invoked. After %d tests.", failure_count);
+static void offline_board_callback(){
+    ESP_LOGE(TAG, "There is an OFFLINE board. Callback invoked.");
 }
 
-void online_board_callback(){
-    ESP_LOGD(TAG, "The node is ONLINE. Callback invoked.");
+static void online_board_callback(){
+    ESP_LOGI(TAG, "The node is back ONLINE. Callback invoked.");
 }
 
-void send_heartbeat() {
+static void send_heartbeat() {
     heartbeat_id++;
     const char msg[] = "HEARTBEAT...";
     heartbeat_received = broadcast_to_siblings_heartbeat(msg, sizeof(msg));
     ESP_LOGD(TAG, "Sent heartbeat %d", heartbeat_id);
 }
 
-void check_heartbeat() {
+static void check_heartbeat() {
     if (!heartbeat_received) {
         failure_count++;
-        ESP_LOGW("HEARTBEAT", "Heartbeat %d not received. Failure #%d", heartbeat_id, failure_count);
+        ESP_LOGE(TAG, "Heartbeat %d not received. Failure #%d", heartbeat_id, failure_count);
         if (failure_count >= MAX_FAILURES) {
             ESP_LOGE(TAG, "Maximum failures reached. Board considered out of service.");
-            offline_board_callback(failure_count);
+            offline_board_callback();
         }
-    } else {
-        failure_count = 0;  // Reset the counter if heartbeat is received
-        online_board_callback();
+    } else
+    {
+        ESP_LOGD(TAG, "The node is ONLINE.");
+        if (failure_count > 0) {
+            failure_count = 0;  // Reset the counter if heartbeat is received and it was failing before
+            online_board_callback();
+        }
     }
 }
 
