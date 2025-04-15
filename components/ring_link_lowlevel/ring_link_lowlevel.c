ea1500c92921dd804462282d12d3e9e920355df8
juanp <jquiroga@fi.uba.ar>
2025-04-15 15:36:09 -0300

    Use interruptions for SPI transactions && Processing times are improved

--- components/ring_link_lowlevel/ring_link_lowlevel.c
+++ components/ring_link_lowlevel/ring_link_lowlevel.c
@@ -4,6 +4,7 @@ static const char* TAG = "==> ring_link_lowlevel";
 
 static SemaphoreHandle_t s_tx_semaphore_handle = NULL;
 static QueueHandle_t ring_link_queue = NULL;
+#include "esp_timer.h"
 
 
 static void ring_link_receive_task(void *pvParameters)
@@ -11,57 +12,63 @@ static void ring_link_receive_task(void *pvParameters)
     esp_err_t rc;
     ring_link_payload_t *payload;
     
-    while (true) {
-        payload = heap_caps_malloc(sizeof(ring_link_payload_t), MALLOC_CAP_DMA);
-        if (payload == NULL) {
-            ESP_LOGE(TAG, "Failed to allocate memory for payload");
-            continue;
-        }
+    // while (true) {
+    //     payload = heap_caps_malloc(sizeof(ring_link_payload_t), MALLOC_CAP_DMA);
+    //     if (payload == NULL) {
+    //         ESP_LOGE(TAG, "Failed to allocate memory for payload");
+    //         continue;
+    //     }
         
-        memset(payload, 0, sizeof(ring_link_payload_t));
-        rc = ring_link_lowlevel_receive_payload(payload);
-        ESP_ERROR_CHECK_WITHOUT_ABORT(rc);
-        if (rc == ESP_OK) {
-            if (xQueueSend(ring_link_queue, &payload, pdMS_TO_TICKS(100)) != pdTRUE) {
-                ESP_LOGW(TAG, "Queue full, payload dropped");
-                free(payload);
-            }
-        } else {
-            free(payload);
-        }
-    }
+    //     memset(payload, 0, sizeof(ring_link_payload_t));
+    //     rc = ring_link_lowlevel_receive_payload(payload);
+    //     ESP_ERROR_CHECK_WITHOUT_ABORT(rc);
+    //     if (rc == ESP_OK) {
+    //         rc = get_spi_msg();
+    //         if (rc == ESP_OK)
+    //         {
+    //             if (xQueueSend(ring_link_queue, &payload, pdMS_TO_TICKS(100)) != pdTRUE) {
+    //                 ESP_LOGW(TAG, "Queue full, payload dropped");
+    //                 free(payload);
+    //             }
+    //         }
+    //     } else {
+    //         free(payload);
+    //     }
+    // }
 }
 
 esp_err_t ring_link_lowlevel_init(QueueHandle_t **queue) {
-    ESP_ERROR_CHECK(RING_LINK_LOWLEVEL_IMPL_INIT());
+    // ESP_ERROR_CHECK(RING_LINK_LOWLEVEL_IMPL_INIT(queue));
     s_tx_semaphore_handle = xSemaphoreCreateMutex();
     BaseType_t ret;
     
-    ret = xTaskCreate(
-        ring_link_receive_task,
-        "ring_link_receive",
-        RING_LINK_READ_MEM_TASK,
-        NULL,
-        (tskIDLE_PRIORITY + 4),
-        NULL
-    );
-    if (ret != pdTRUE) {
-        ESP_LOGE(TAG, "Failed to create receive task");
-        return ESP_FAIL;
-    }
+    // ret = xTaskCreate(
+    //     ring_link_receive_task,
+    //     "ring_link_receive",
+    //     RING_LINK_READ_MEM_TASK,
+    //     NULL,
+    //     (tskIDLE_PRIORITY + 4),
+    //     NULL
+    // );
+    // if (ret != pdTRUE) {
+    //     ESP_LOGE(TAG, "Failed to create receive task");
+    //     return ESP_FAIL;
+    // }
 
-    ring_link_queue = xQueueCreate(RING_LINK_READ_QUEUE_SIZE, sizeof(ring_link_payload_t*));
-    *queue = &ring_link_queue;
-    if (ring_link_queue == NULL) {
-        ESP_LOGE(TAG, "Failed to create queue");
-        return ESP_FAIL;
-    }
+    // ring_link_queue = xQueueCreate(RING_LINK_READ_QUEUE_SIZE, sizeof(ring_link_payload_t*));
+    // ring_link_queue = NULL;
+    // *queue = &ring_link_queue;
+    // if (ring_link_queue == NULL) {
+    //     ESP_LOGE(TAG, "Failed to create queue");
+    //     return ESP_FAIL;
+    // }
 
     return ESP_OK;
 }
 
 esp_err_t ring_link_lowlevel_transmit_payload(ring_link_payload_t *p)
 {
+    int64_t transaction_start_time_ = esp_timer_get_time();
     esp_err_t rc;
     ESP_LOGD(TAG, "Payload puesto en queue:");
     ESP_LOGD(TAG, "  id: %d", p->id);
@@ -70,12 +77,16 @@ esp_err_t ring_link_lowlevel_transmit_payload(ring_link_payload_t *p)
     ESP_LOGD(TAG, "  len: %d", p->len);
     ESP_LOGD(TAG, "  src_id: %d", p->src_id);
     ESP_LOGD(TAG, "  dst_id: %d\n", p->dst_id);
-    if( xSemaphoreTake( s_tx_semaphore_handle, ( TickType_t ) 10 ) == pdTRUE )
+    if( xSemaphoreTake( s_tx_semaphore_handle, ( TickType_t ) 1000 ) == pdTRUE )
     {
         rc = RING_LINK_LOWLEVEL_IMPL_TRANSMIT(p, sizeof(ring_link_payload_t));
         xSemaphoreGive( s_tx_semaphore_handle );
+        int64_t end_time = esp_timer_get_time();
+        int64_t duration = end_time - transaction_start_time_;
+        ESP_LOGW(TAG, "Mensaje ring_link_lowlevel_transmit_payload: en %lld μs", duration);
         return rc;
     }
+    printf("Falla la transmision");
     ESP_LOGE(TAG, "Could not adquire Mutex...");
     return ESP_FAIL;
 }
--- components/ring_link_lowlevel/ring_link_payload.c
+++ components/ring_link_lowlevel/ring_link_payload.c
@@ -1,12 +1,19 @@
 #include "ring_link_payload.h"
+#include "esp_timer.h"
 
 static const char* TAG = "==> ring_link_payload";
 
 
 bool ring_link_payload_is_for_device(ring_link_payload_t *p)
 {
-    return p->dst_id == config_get_id() \
+    bool result;
+    int64_t transaction_start_time_ = esp_timer_get_time();
+    result = p->dst_id == config_get_id() \
         || (p->dst_id == CONFIG_ID_ANY);
+    int64_t end_time = esp_timer_get_time();
+    int64_t duration = end_time - transaction_start_time_;
+    ESP_LOGW(TAG, "Mensaje ring_link_payload_is_for_device: en %lld μs", duration);
+    return result;
 }
 
 bool ring_link_payload_is_from_device(ring_link_payload_t *p)
